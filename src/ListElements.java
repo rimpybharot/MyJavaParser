410d13aff2063da6284d1f6ad0352b6d4160cae2
Rimpy Bharot <rbverses21@gmail.com>
2017-03-17 01:15:56 -0700

    code update

--- src/ListElements.java
+++ src/ListElements.java
@@ -10,106 +10,46 @@ import com.github.javaparser.ast.expr.SimpleName;
 import com.github.javaparser.ast.visitor.*;
 
 public class ListElements{
-	
+
 	private String className;
- 
-    public void listClasses(File file) throws ParseException {
-            try {
-                new VoidVisitorAdapter<Object>() {
 
-					@Override
-                    public void visit(ClassOrInterfaceDeclaration n, Object arg) {
-                        super.visit(n, arg);
-                        setClassName(n.getName());
-                    }
-                }.visit(JavaParser.parse(file), null);
-            } catch (IOException e) {
-                new RuntimeException(e);
-            }
-        }
+	public void listClasses(File file) throws ParseException {
+		try {
+			new VoidVisitorAdapter<Object>() {
 
- 
-    public void setClassName(SimpleName sn){
-    	this.className = "class " + sn.toString();
-    }
-    
-    public String getClassName(){
-    	return this.className;
-    }
-    
-//    public Map<String, Object> parseFile(File file) throws IOException, ParseException {
-//    	final CompilationUnit unit = JavaParser.parse(file);
-//    	final List<FieldDeclaration> fields = new ArrayList<FieldDeclaration>();
-//    	final List<MethodDeclaration> methods = new ArrayList<MethodDeclaration>();
-//    	final Map<String, Object> ret = new HashMap<String, Object>();
-//    	ret.put("fields", fields);
-//    	ret.put("methods", methods);
-//
-//    	new VoidVisitorAdapter<Object>() {
-//    		@Override
-//    		public void visit(ClassOrInterfaceDeclaration node, Object arg) {
-//    			if (ret.containsKey("classOrInterface")) {
-//    				return;
-//    			}
-//    			
-//    			ret.put("classOrInterface", node);
-////    			super.visit(node, arg);
-//    		}
-//
-//    		@Override
-//    		public void visit(FieldDeclaration node, Object arg) {
-//    			fields.add(node);
-////    			super.visit(node, arg);
-//    		}
-//
-//    		@Override
-//    		public void visit(MethodDeclaration node, Object arg) {
-//    			methods.add(node);
-////    			super.visit(node, arg);
-//    		}
-//    	}.visit(unit, null);
-//
-//    	return ret;
-//    }
-//     
-   
-	public static void main(String[] args) throws ParseException, IOException {
-        
-    	FileHandling1 fh = new FileHandling1(args[0]);
-    	ListElements lc = new ListElements();
-    	List<File> javafiles = fh.getJavaFiles();
-    	VariableParser vp = new VariableParser();
-
-
-    	for (File file : javafiles){
-    		
-    		
-    		
-//    		System.out.println(file.toString().substring(file.toString().lastIndexOf("\\")+1));
-    		
-    		lc.listClasses(file);
-//    		lc.listVariables(file);
-//
-//    		MethodDeclarationChecker m = new MethodDeclarationChecker(file);
-//    		m.getGetters();
-//    		m.getSetters();
-    		vp.listVariables(file);
-    		String fileName = (file.getName()).replaceAll("java", "txt");
-
-        	try{
-        	    PrintWriter writer = new PrintWriter(fileName, "UTF-8");
-        	    writer.println(lc.getClassName());
-        	    writer.close();
-        	} catch (IOException e) {
-        	   System.err.println("IO Exception");
-        	}
-        	File f = new File(fileName);
-        	PlantUMLDiargramGenerator pdg = new PlantUMLDiargramGenerator(f);
-
-    	}
-		    
-
-    	
-    	
+				@Override
+				public void visit(ClassOrInterfaceDeclaration n, Object arg) {
+					super.visit(n, arg);
+					setClassName(n.getName());
+				}
+			}.visit(JavaParser.parse(file), null);
+		} catch (IOException e) {
+			new RuntimeException(e);
+		}
 	}
-    }
+
+
+	public void setClassName(SimpleName sn){
+		this.className = "class " + sn.toString();
+	}
+
+	public String getClassName(){
+		return this.className;
+	}
+
+	public static void main(String[] args) throws ParseException, IOException {
+		FileHandling1 fh = new FileHandling1(args[0]);
+		ListElements lc = new ListElements();
+		List<File> javafiles = fh.getJavaFiles();
+
+
+		for (File file : javafiles){
+			
+
+			PlantUMLDiargramGenerator pm = new PlantUMLDiargramGenerator(file);
+
+			
+//
+		}
+	}
+}
--- src/PlantUMLDiargramGenerator.java
+++ src/PlantUMLDiargramGenerator.java
@@ -1,41 +1,67 @@
-//import java.io.BufferedReader;
-//import java.io.File;
-//import java.io.FileNotFoundException;
-//import java.io.FileOutputStream;
-//import java.io.FileReader;
-//import java.io.IOException;
-//import java.io.StringReader;
-//import java.nio.file.Files;
-//
-//import net.sourceforge.plantuml.FileFormat;
-//import net.sourceforge.plantuml.FileFormatOption;
-//import net.sourceforge.plantuml.SourceStringReader;
-//
-//public class PlantUMLDiargramGenerator {
-//
-//
-//	public PlantUMLDiargramGenerator(File file) throws IOException {
-//		// TODO Auto-generated constructor stub
-//
-//		String plantUmlSource = new String();
-//
-//		plantUmlSource += "@startuml\n";
-//		FileReader fr = new FileReader(file);
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.nio.file.Files;
+
+import com.github.javaparser.ParseException;
+
+import net.sourceforge.plantuml.FileFormat;
+import net.sourceforge.plantuml.FileFormatOption;
+import net.sourceforge.plantuml.SourceStringReader;
+
+public class PlantUMLDiargramGenerator {
+
+
+	public PlantUMLDiargramGenerator(File file) throws IOException, ParseException {
+		// TODO Auto-generated constructor stub
+
+		ClassesDeclarationChecker cdc = new ClassesDeclarationChecker(file);
+		VariableParser vp = new VariableParser();
+
+		
+		
+		String plantUmlSource = new String();
+
+		plantUmlSource += "@startuml\nskinparam classAttributeIconSize 0\n";
+		plantUmlSource += cdc.getClassName().toString();
+		plantUmlSource += "{\n";
+		plantUmlSource += "\n"+vp.listVariables(file)+"\nlove";
+		plantUmlSource += "\n}";
+		
+		FileReader fr = new FileReader(file);
+		
 //		@SuppressWarnings("resource")
 //		BufferedReader br = new BufferedReader(fr);
 //		plantUmlSource += br.readLine();
-//		plantUmlSource += "\n@enduml";
-//
-//		SourceStringReader reader = new SourceStringReader(plantUmlSource);
-//		
-//		
+		plantUmlSource += "\n@enduml\n";
+		
+		
+//		plantUmlSource +=String.format("%n");
+//		plantUmlSource+="}";
 //		System.out.println(plantUmlSource);
-//
-//		String fileName = (file.toString()).replace("txt","png");
-//
-//		FileOutputStream output = new FileOutputStream(new File(fileName));
-//
-//		reader.generateImage(output, new FileFormatOption(FileFormat.PNG));
-//	}
-//
-//}
+
+		
+		String fileName = "target\\" + (file.getName()).replaceAll("java", "png");
+
+
+
+		
+
+
+		SourceStringReader reader = new SourceStringReader(plantUmlSource);
+		
+		
+		System.out.println(plantUmlSource);
+
+
+		FileOutputStream output = new FileOutputStream(new File(fileName));
+
+		reader.generateImage(output, new FileFormatOption(FileFormat.PNG));
+	}
+
+}

0103c91bcfab68bc4bdcd6c570998add38a7f683
rimpybharot <rbverses21@gmail.com>
2017-03-16 18:19:41 -0700

    Get Class Names and generate diagram
    We have parsed classes to get the class name, written it to the text file, read it for plantuml and generated only the class block in the diagram.

--- ListElements.java
+++ ListElements.java
@@ -1,23 +1,26 @@
-/* @author Rimpy Bharot*/
+/* @author Rimpy Bharot */
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
-
+import java.io.PrintWriter;
+import java.util.*;
 import com.github.javaparser.*;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.expr.MethodCallExpr;
-import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.ast.expr.SimpleName;
+import com.github.javaparser.ast.visitor.*;
 
-public class ListClasses{
+public class ListElements{
+	
+	private String className;
  
     public void listClasses(File file) throws ParseException {
             try {
                 new VoidVisitorAdapter<Object>() {
-                    @Override
+
+					@Override
                     public void visit(ClassOrInterfaceDeclaration n, Object arg) {
                         super.visit(n, arg);
-                        System.out.println(" Class " + n.getName());
+                        setClassName(n.getName());
                     }
                 }.visit(JavaParser.parse(file), null);
             } catch (IOException e) {
@@ -26,34 +29,87 @@ public class ListClasses{
         }
 
  
-    
-    public void listMethodCalls(File file) throws ParseException {
-            try {
-                new VoidVisitorAdapter<Object>() {
-                    @Override
-                    public void visit(MethodCallExpr n, Object arg) {
-                        super.visit(n, arg);
-                        System.out.println(" [L " + n.getBegin() + "] " + n);
-                    }
-                }.visit(JavaParser.parse(file), null);
-                System.out.println(); // empty line
-            } catch (IOException e) {
-                new RuntimeException(e);
-            }
-        }
-    
-
-	public static void main(String[] args) throws ParseException {
-        
-    	FileHandling fh = new FileHandling(args[0]);
-    	ListClasses lc = new ListClasses();
-    	List<File> javafiles = fh.getJavaFiles();
-    	for (File file : javafiles)
-    	{
-    		lc.listClasses(file);
-    		lc.listMethodCalls(file);
-    	}
-    	
-    	
+    public void setClassName(SimpleName sn){
+    	this.className = "class " + sn.toString();
     }
-}
+    
+    public String getClassName(){
+    	return this.className;
+    }
+    
+//    public Map<String, Object> parseFile(File file) throws IOException, ParseException {
+//    	final CompilationUnit unit = JavaParser.parse(file);
+//    	final List<FieldDeclaration> fields = new ArrayList<FieldDeclaration>();
+//    	final List<MethodDeclaration> methods = new ArrayList<MethodDeclaration>();
+//    	final Map<String, Object> ret = new HashMap<String, Object>();
+//    	ret.put("fields", fields);
+//    	ret.put("methods", methods);
+//
+//    	new VoidVisitorAdapter<Object>() {
+//    		@Override
+//    		public void visit(ClassOrInterfaceDeclaration node, Object arg) {
+//    			if (ret.containsKey("classOrInterface")) {
+//    				return;
+//    			}
+//    			
+//    			ret.put("classOrInterface", node);
+////    			super.visit(node, arg);
+//    		}
+//
+//    		@Override
+//    		public void visit(FieldDeclaration node, Object arg) {
+//    			fields.add(node);
+////    			super.visit(node, arg);
+//    		}
+//
+//    		@Override
+//    		public void visit(MethodDeclaration node, Object arg) {
+//    			methods.add(node);
+////    			super.visit(node, arg);
+//    		}
+//    	}.visit(unit, null);
+//
+//    	return ret;
+//    }
+//     
+   
+	public static void main(String[] args) throws ParseException, IOException {
+        
+    	FileHandling1 fh = new FileHandling1(args[0]);
+    	ListElements lc = new ListElements();
+    	List<File> javafiles = fh.getJavaFiles();
+    	VariableParser vp = new VariableParser();
+
+
+    	for (File file : javafiles){
+    		
+    		
+    		
+//    		System.out.println(file.toString().substring(file.toString().lastIndexOf("\\")+1));
+    		
+    		lc.listClasses(file);
+//    		lc.listVariables(file);
+//
+//    		MethodDeclarationChecker m = new MethodDeclarationChecker(file);
+//    		m.getGetters();
+//    		m.getSetters();
+    		vp.listVariables(file);
+    		String fileName = (file.getName()).replaceAll("java", "txt");
+
+        	try{
+        	    PrintWriter writer = new PrintWriter(fileName, "UTF-8");
+        	    writer.println(lc.getClassName());
+        	    writer.close();
+        	} catch (IOException e) {
+        	   System.err.println("IO Exception");
+        	}
+        	File f = new File(fileName);
+        	PlantUMLDiargramGenerator pdg = new PlantUMLDiargramGenerator(f);
+
+    	}
+		    
+
+    	
+    	
+	}
+    }

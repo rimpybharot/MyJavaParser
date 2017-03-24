bf233ac4031e25c442d9b73571455e806f18ebee
Rimpy Bharot <rbverses21@gmail.com>
2017-03-24 11:33:08 -0500

    additional checks in variable parser

--- src/VariableParser.java
+++ src/VariableParser.java
@@ -9,8 +9,10 @@ import java.util.Map;
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParseException;
 import com.github.javaparser.ast.Modifier;
+import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.AnnotationDeclaration;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.comments.Comment;
@@ -20,13 +22,12 @@ import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import net.sourceforge.plantuml.graph.LenghtLinkComparator;
 
 public class VariableParser {
+	private List<String> variables;
 
-	private static  Map<String, String[]> variableMap = new HashMap<>();
 
+	public void listVariables(ClassOrInterfaceDeclaration cd) {
+		variables = new ArrayList<String>();
 
-	public String listVariables(File file) throws ParseException, IOException {
-
-		List<String> fields = new ArrayList<String>();
 
 		new VoidVisitorAdapter<Object>() {
 
@@ -35,30 +36,48 @@ public class VariableParser {
 			String modifier = null;
 
 			public void visit(FieldDeclaration n, Object arg) {
+//				System.out.println(n.toString());
+				List<String> fields = new ArrayList<String>();
+
+				
 				name = n.getVariable(0).toString();
 				type = n.getElementType().toString();
 				EnumSet<Modifier> e = n.getModifiers();
+				modifier = "";
 				for (Modifier m : e){
 					modifier = (m.toString()).toLowerCase();
+//					System.out.println("modifier " + modifier);
 				}
-				if(modifier != null){
+//				if(modifier != null){
 					fields.add(modifier);
-				}
+//				}
 				if(name!=null){
 					fields.add(name);	
 				}
 				if(type!=null){
 					fields.add(type);
 				}
+				
+				setVariables(createVarString(fields));
+//				System.out.println(createVarString(fields));
 
 			}
-		}.visit(JavaParser.parse(file), null);
-//		System.out.println(var(fields));
-		return var(fields);
+		}.visit(cd, null);
 }
 
 
-	public String var(List<String> variableDefinition){
+	public void setVariables(String string) {
+		// TODO Auto-generated method stub
+		this.variables.add(string);
+		
+	}
+	
+	public List<String> getVariables(){
+		return this.variables;
+	}
+
+
+	public String createVarString(List<String> variableDefinition){
 		String finalVariable = null;
 
 		if(variableDefinition.size()>0){
@@ -67,6 +86,7 @@ public class VariableParser {
 			case "private" : variableDefinition.set(0, "-"); break;
 			case "protected" : variableDefinition.set(0, "#"); break;
 			case "package private" : variableDefinition.set(0, "~"); break;
+			case "": variableDefinition.set(0, "~"); break;
 			default: variableDefinition.set(0, "-"); break;
 			}
 			String name = variableDefinition.get(2).toString();
@@ -97,5 +117,6 @@ public class VariableParser {
 
 
 	}
+
 
 }

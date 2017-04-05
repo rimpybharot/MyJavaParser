009768cc4fe8142bc19ecd36c00cbd8cb2580964
Rimpy Bharot <rbverses21@gmail.com>
2017-04-05 14:21:23 -0700

    Add main here

--- src/PlantUMLDiargramGenerator.java
+++ src/PlantUMLDiargramGenerator.java
@@ -4,11 +4,23 @@ import java.io.IOException;
 import net.sourceforge.plantuml.FileFormat;
 import net.sourceforge.plantuml.FileFormatOption;
 import net.sourceforge.plantuml.SourceStringReader;
+import java.util.*;
+import com.github.javaparser.*;
 
 
 
 public class PlantUMLDiargramGenerator {
 
+	public static void main(String[] args) throws ParseException, IOException {
+		FileHandling1 fh = new FileHandling1(args[0]);
+		List<File> javafiles = fh.getJavaFiles();
+
+		callers p = new callers(javafiles);
+
+		new PlantUMLDiargramGenerator(p.getumlTextNoation());
+
+
+	}
 
 	public PlantUMLDiargramGenerator(String plantumlTextNotation) throws IOException{
 		// TODO Auto-generated constructor stub

70aaa72cd5e97ba9060bdbfd0a8ad3e88117161b
Rimpy Bharot <rbverses21@gmail.com>
2017-04-09 23:13:30 -0700

    Dependency of classes with interfaces by the virtue of variabe creation

---
+++ src/VariableDeclaration.java
@@ -0,0 +1,37 @@
+import java.util.List;
+
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.VariableDeclarationExpr;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
+public class VariableDeclaration {
+	
+	private String methodDependency = null;
+
+	public VariableDeclaration() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getDependenciesFromMethods(MethodDeclaration method, List<String> interfaces) {
+
+		new VoidVisitorAdapter<Object>() {
+			public void visit(VariableDeclarationExpr n, Object arg) {
+
+			
+				if(interfaces.contains(n.getElementType().toString())){
+					methodDependency = n.getElementType().toString();
+				}
+				
+			}
+		}.visit(method, null);
+
+		return methodDependency;
+	}
+
+
+
+
+
+}

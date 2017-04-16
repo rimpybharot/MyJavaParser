b10b8d10c7a741d0f67ff361e4fb424b2e8a9041
Rimpy Bharot <rbverses21@gmail.com>
2017-04-15 18:59:29 -0700

    Final Diagram generation code

--- src/PlantUMLDiagramGenerator.java
+++ src/PlantUMLDiagramGenerator.java
@@ -1,33 +1,24 @@
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import net.sourceforge.plantuml.FileFormat;
 import net.sourceforge.plantuml.FileFormatOption;
 import net.sourceforge.plantuml.SourceStringReader;
 import java.util.*;
 import com.github.javaparser.*;
-
-
-
 public class PlantUMLDiagramGenerator {
 
-	public static void main(String[] args) throws ParseException, IOException {
+	public static void main(String[] args) throws ParseException, IOException, ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		FileHandling fh = new FileHandling(args[0]);
 	    File f = new File(args[0].toString());
-		
 		List<File> javafiles = fh.getJavaFiles();
-
-//		callers p = new callers(javafiles);
 		PlantUMLNotationMaker p = new PlantUMLNotationMaker(javafiles);
-
 		new PlantUMLDiagramGenerator(p.getumlTextNotation(), f.getName());
-
-
 	}
 
 	public PlantUMLDiagramGenerator(String plantumlTextNotation, String filename) throws IOException{
 		new File("output").mkdir();
-
 		String fileName = "output\\"+filename+".png";
 		System.out.println("Output file located at " + fileName);
 		SourceStringReader reader = new SourceStringReader(plantumlTextNotation);

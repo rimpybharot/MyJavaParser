bdb4e612cc4e1f979b0c3b3b58964a0cc61a97a1
Rimpy Bharot <rbverses21@gmail.com>
2017-04-21 12:58:44 -0700

    refactor

--- src/PlantUMLDiagramGenerator.java
+++ src/PlantUMLDiagramGenerator.java
@@ -1,29 +1,63 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+import com.github.javaparser.ParseException;
+
 import net.sourceforge.plantuml.FileFormat;
 import net.sourceforge.plantuml.FileFormatOption;
 import net.sourceforge.plantuml.SourceStringReader;
-import java.util.*;
-import com.github.javaparser.*;
 public class PlantUMLDiagramGenerator {
 
-	public static void main(String[] args) throws ParseException, IOException, ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+	public static void main(String[] args){
 		FileHandling fh = new FileHandling(args[0]);
-	    File f = new File(args[0].toString());
+		File f = new File(args[0].toString());
+		if(f.exists()){
 		List<File> javafiles = fh.getJavaFiles();
-		PlantUMLNotationMaker p = new PlantUMLNotationMaker(javafiles);
-		new PlantUMLDiagramGenerator(p.getumlTextNotation(), f.getName());
+		PlantUMLNotationMaker p;
+		try {
+			p = new PlantUMLNotationMaker(javafiles);
+			if(f.getName()!=null || !f.getName().isEmpty()){
+				try {
+					new PlantUMLDiagramGenerator(p.getumlTextNotation(), f.getName());
+				} catch (SecurityException | IllegalArgumentException e) {
+					System.out.println("SecurityException file path");
+					return;
+				}
+			}
+			else{
+				System.out.println("Path is not correct");
+				return;
+			}
+		} catch (ParseException | IOException e) {
+			System.out.println("Check file path");
+			return;
+		}
+	}
+		else{
+			System.out.println("File does not exist");
+		}
+
 	}
 
-	public PlantUMLDiagramGenerator(String plantumlTextNotation, String filename) throws IOException{
+	public PlantUMLDiagramGenerator(String plantumlTextNotation, String filename){
 		new File("output").mkdir();
 		String fileName = "output\\"+filename+".png";
 		System.out.println("Output file located at " + fileName);
 		SourceStringReader reader = new SourceStringReader(plantumlTextNotation);
-		// System.out.println(plantumlTextNotation);
-		FileOutputStream output = new FileOutputStream(new File(fileName));
-		reader.generateImage(output, new FileFormatOption(FileFormat.PNG));
+		FileOutputStream output;
+		try {
+			output = new FileOutputStream(new File(fileName));
+			try {
+				reader.generateImage(output, new FileFormatOption(FileFormat.PNG));
+			} catch (IOException e) {
+				System.out.println("something wrong with creating image, please checkm your settings");
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("something wrong with creating image, please checkm your settings");
+		}
+
 	}
 }

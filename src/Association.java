b1dd13f5a2cb6bbf6abaa1372b662341d94ac51b
Rimpy Bharot <rbverses21@gmail.com>
2017-04-21 12:56:48 -0700

    remove unnecesary sysouts

--- src/Association.java
+++ src/Association.java
@@ -12,36 +12,18 @@ public class Association {
 	private HashMap<String, ArrayList<String>> associationsList = new HashMap<>();
 
 	public Association(HashMap<String, ArrayList<String>> associationsList) {
-
 		this.associationsList = associationsList;
 		for(Entry<String, ArrayList<String>> association : associationsList.entrySet()){
-
 			ArrayList<String> otherClasses = new ArrayList<>();
-
 			for(Entry<String, ArrayList<String>> association1 : associationsList.entrySet()){
-
 				otherClasses.add(association1.getKey().toString());
-
 			}
 			otherClasses.remove(association.getKey());
-
-			//System.out.println("Now " + association.getKey()+" \nOthers " + otherClasses);
-
-
 			ArrayList<String> otherClassInAssociation = association.getValue();
-
-
 			for(int i=0;i<otherClassInAssociation.size();i=i+3){
-
-				//System.out.println("get rel with other "+ otherClassInAssociation.get(i));
-
 				String key = otherClassInAssociation.get(i);
-
 				if(otherClasses.contains(otherClassInAssociation.get(i))){
-					
 					if(this.associationsList.get(key).contains(association.getKey())){
-						//System.out.println("bi");
-						
 						int otherRel = associationsList.get(key).indexOf(association.getKey());
 						String m1 = associationsList.get(key).get(otherRel+1);
 						
@@ -49,45 +31,24 @@ public class Association {
 								m1, otherClassInAssociation.get(i+1));
 					}
 					else{
-						//System.out.println("uni but present somewhere");
 						setRelationShip(association.getKey(), otherClassInAssociation.get(i), "" , 
 								otherClassInAssociation.get(i+1));
 					}
 				}
 				else{
-					//System.out.println("uni");
-
 					setRelationShip(association.getKey(), otherClassInAssociation.get(i), "" , 
 							otherClassInAssociation.get(i+1));
 				}
-
 			}
-
-
-
 		}
-
-
-
 	}
 
 
 
 	private void setRelationShip(String rel1, String rel2, String rel1M, String rel2M) {
-
-		//System.out.println("new \n Lets make relations");
-		
-		//System.out.println(rel1);
-		//System.out.println(rel2);
-		//System.out.println(rel1M);
-		//System.out.println(rel2M);
-
-		
 		boolean relationExists = false;
 		if(!this.finalRel.isEmpty()){
 			for(ArrayList<String> someRel : this.finalRel){
-				
-				//System.out.println("final rel not empty");
 
 				Pattern p1 = Pattern.compile("\\b"+rel1+"\\b");
 				Matcher m1 = p1.matcher(someRel.get(0));
@@ -100,24 +61,16 @@ public class Association {
 
 				Pattern r1 = Pattern.compile("\""+rel1M+"\"--");
 				Matcher rm1 = r1.matcher(someRel.get(1));
-				//System.out.println("\""+rel1M+"\"--");
-				//System.out.println(someRel.get(1));
-				
 				
 				Pattern r2 = Pattern.compile("\""+rel2M+"\"");
 				Matcher rm2 = r2.matcher(someRel.get(2));
-				//System.out.println("\""+rel2M+"\"");
-				//System.out.println(someRel.get(2));
 
 				Pattern r3 = Pattern.compile("\""+rel2M+"\"--");
 				Matcher rm3 = r3.matcher(someRel.get(1));
-				//System.out.println("\""+rel2M+"\"--");
-				//System.out.println(someRel.get(1));
 
 				Pattern r4 = Pattern.compile("\""+rel1M+"\"");
 				Matcher rm4 = r4.matcher(someRel.get(2));
-				//System.out.println("\""+rel1M+"\"");
-				//System.out.println(someRel.get(2));
+
 
 				if((m1.find() && m2.find() && rm1.find() && rm2.find())
 				|| ( m3.find() && m4.find() && rm3.find() && rm4.find())){
@@ -147,16 +100,9 @@ public class Association {
 				rl.add(2, rel2M);
 			}
 			rl.add(3, rel2);
-			//System.out.println("Relation made");
 			relationExists = false;
-
-			//System.out.println(rl);
 			this.finalRel.add(rl);
 		}
-
-//		for(ArrayList<String> rl : this.finalRel){
-//			System.out.println(rl);
-//		}
 	}
 
 
@@ -165,9 +111,6 @@ public class Association {
 		this.finalRel.removeAll(Collections.singleton(null));
 		return this.finalRel;
 	}
-
-
-
 }
 
 

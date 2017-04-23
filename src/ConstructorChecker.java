22ab72ef3591cad1f06e136522b73a390f7ac582
Rimpy Bharot <rbverses21@gmail.com>
2017-04-22 20:41:11 -0700

    add code to show more than one parameters in constructor

--- src/ConstructorChecker.java
+++ src/ConstructorChecker.java
@@ -42,29 +42,63 @@ public class ConstructorChecker {
 	}
 	public String constructorStringCreator(ConstructorDeclaration n){
 
+//		String p1 ="";
+//		parameters = n.getParameters();
+//
+//		String name = null;
+//
+//
+//		for(Parameter p : parameters){
+//			//			//////////System.out.println("parameters " + p.getType());
+//			p1 += p.getNameAsString() + ": " + p.getType();
+//
+//		}
+//		name = n.getNameAsString() + "("+p1+")";
+//		String modifier = "";
+//		if(n.isPrivate()){
+//			modifier ="-";
+//		}
+//		else if(n.isPublic()){
+//			modifier ="+";
+//		}
+//		else if(n.isProtected()){
+//			modifier ="#";
+//		}
+//		return modifier +" " + name;
+		
+		
+
+		
+
+		String type = null;
+		String name = null;
+		String modifier = null;
+
+
 		String p1 ="";
 		parameters = n.getParameters();
+		
+		
 
-		String name = null;
 
+		boolean first = true;
+		
+		
+		for (Parameter p : parameters) {
+			if (first==false) {
+				System.out.println(p.getNameAsString());
+				p1 += " , "+p.getNameAsString() + ": " + p.getType();
+			}
+			else{
+				p1 += p.getNameAsString() + ": " + p.getType();
+			}
 
-		for(Parameter p : parameters){
-			//			//////////System.out.println("parameters " + p.getType());
-			p1 += p.getNameAsString() + ": " + p.getType();
-
+			first = false;
 		}
 		name = n.getNameAsString() + "("+p1+")";
-		String modifier = "";
-		if(n.isPrivate()){
-			modifier ="-";
-		}
-		else if(n.isPublic()){
-			modifier ="+";
-		}
-		else if(n.isProtected()){
-			modifier ="#";
-		}
-		return modifier +" " + name;
+		modifier = "+";
+		return modifier +" " + name + " : " + type;
+
 
 	}
 }
